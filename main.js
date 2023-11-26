fecf77cc02b8371885e4fd8afa08971f073169cd
Diego <jdiegocalderon04@hotmail.com>
2023-11-25 21:16:03 -0500

    DisplayDetails

--- main.js
+++ main.js
@@ -5,7 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const nextPageButton = document.getElementById('nextPage');
     const currentPageSpan = document.getElementById('currentPage');
     const totalPagesSpan = document.getElementById('totalPages');
+    const searchInput = document.getElementById('searchInput');
+    const searchButton = document.getElementById('searchButton');
     let currentPage = 1;
+    let characterDetailsCache = {}; // Almacena los detalles del personaje por ID
+
+    searchButton.addEventListener('click', () => searchCharacterById());
+    searchInput.addEventListener('input', () => {
+        if (!searchInput.value.trim()) {
+            // Limpiar los detalles si el campo de búsqueda está vacío
+            characterDetail.innerHTML = '';
+            characterDetail.style.display = 'none';
+        }
+    });
 
     prevPageButton.addEventListener('click', () => changePage('prev'));
     nextPageButton.addEventListener('click', () => changePage('next'));
@@ -13,8 +25,8 @@ document.addEventListener('DOMContentLoaded', function () {
     getCharacters(currentPage).then(data => {
         populateTable(characterTable, data.results, character => {
             displayCharacterDetail(characterDetail, character);
-        });-
-        updatePagination(data.info); // Agregamos esta línea para actualizar la información de paginación
+        });
+        updatePagination(data.info);
     });
 
     function changePage(direction) {
@@ -28,12 +40,34 @@ document.addEventListener('DOMContentLoaded', function () {
             populateTable(characterTable, data.results, character => {
                 displayCharacterDetail(characterDetail, character);
             });
-            updatePagination(data.info); // Actualizamos la información de paginación
+            updatePagination(data.info);
         });
     }
 
     function updatePagination(info) {
         currentPageSpan.textContent = `${currentPage}`;
         totalPagesSpan.textContent = ` / ${info.pages}`;
+    }
+
+    function searchCharacterById() {
+        const characterId = parseInt(searchInput.value);
+
+        if (!isNaN(characterId)) {
+            if (characterDetailsCache[characterId]) {
+                // Si los detalles del personaje ya están en caché, mostrarlos directamente
+                displayCharacterDetail(characterDetail, characterDetailsCache[characterId]);
+            } else {
+                getCharacterDetails(characterId, character => {
+                    // Almacenar los detalles del personaje en caché
+                    characterDetailsCache[characterId] = character;
+
+                    // Limpiar el campo de búsqueda
+                    searchInput.value = '';
+
+                    // Mostrar los detalles del personaje
+                    displayCharacterDetail(characterDetail, character);
+                });
+            }
+        }
     }
 });
